2a587d6d77f5bbf33cda602cf2460fc24f47c122
Albertpm1 <petermburunganga98@gmail.com>
2023-03-06 17:41:30 +0300

    complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,37 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+    
+  get "/movies" do
+    directors = Movie.all 
+    directors.to_json
   end
-
+  get "/movies/:id" do
+    directors = Movie.find_by(id: params[:id])
+    directors.to_json
+  end
+  get "/movies/:title" do
+    
+    movies = Movie.find_by(title: params[:title])
+    movies.to_json
+    end
+    get "/movies/:heading" do
+    
+      movies = Movie.find_by(heading: params[:heading])
+      movies.to_json
+      end
+      delete "/movies/:id" do
+        movies = Movie.find_by(id: params[:id])
+        movies.destroy
+      end
+      post '/users' do
+        user = User.create(title: params[:title], poster: params[:poster], director_id: params[:director_id])
+        if user.valid?
+          status 201
+          user.to_json
+        else
+          status 422
+          user.errors.to_json
+        end
+      end
+ 
 end
